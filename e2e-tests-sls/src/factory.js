5062399afa17bb729600d115e0342b21f999d1ae
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-04-17 17:03:34 -0300

    start test e2e-test-sls

---
+++ e2e-tests-sls/src/factory.js
@@ -0,0 +1,23 @@
+import { CreateBucketCommand, DeleteBucketCorsCommand, ListBucketsCommand, S3Client } from '@aws-sdk/client-s3'
+const s3Config = {
+  forcePathStyle: true,
+  region: 'us-east-1'
+}
+
+const isLocal = process.env.IS_OFFLINE
+
+if (isLocal) {
+  const host = process.env.LOCALSTACK ?? 'localhost'
+  s3Config.endpoint = `http://${host}:4566`
+}
+
+const s3Client = new S3Client(s3Config)
+
+const s3 = {
+  listBuckets: args => {
+    return s3Client.send(new ListBucketsCommand(args))
+  },
+  createBucket: args => s3Client.send(new CreateBucketCommand(args)),
+  deleteBucket: args => s3Client.send(new DeleteBucketCorsCommand(args))
+
+}
